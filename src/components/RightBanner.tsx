6bb0eec7b6c05e29f3289c3c73e95b0164e16d43
DewmiSilvaa <dewmi78@gmail.com>
2025-03-10 05:41:29 +0530

    commits

--- src/components/RightBanner.tsx
+++ src/components/RightBanner.tsx
@@ -1,11 +1,12 @@
 import { FadeIn } from "./FadeIn";
+import {dewmi} from "../assets/images/dewmi.png"
 
 const RightBanner = () => {
   return (
     <FadeIn className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        src="src/assests/images/dewmi.png"
+        src={dewmi}
         alt="me2"
       />
       <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#0B1120] shadow-shadowOne flex justify-center items-center"></div>
